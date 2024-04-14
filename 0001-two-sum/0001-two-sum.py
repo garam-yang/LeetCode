10a3e8489066823d4a12f43a0bb39048d824e8e1
garam-yang <149549078+garam-yang@users.noreply.github.com>
2024-04-14 18:43:58 +0900

    Time: 60 ms (61.62%), Space: 17.8 MB (16.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}  # 숫자와 그 인덱스를 매핑하기 위한 해시 테이블
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
